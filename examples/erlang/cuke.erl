3de566fbdbfbb7b805d5cfa905bbfc004c8f9e23
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-04-28 19:18:15 -0500

    Some more erlang ideas

--- examples/erlang/cuke.erl
+++ examples/erlang/cuke.erl
@@ -1,5 +1,16 @@
+% dialyser
 -module(cuke).
 -export([given/1]).
 
-given({i, have, X, cukes}) ->
+% Gherkin: Given I have 7 cukes
+% given(['I', have, 7, cukes])
+
+% [7]
+given(['I', have, X, cukes]) ->
+	X;
+
+given(['I', have, X, wives, in, Y]) ->
 	X.
+		
+then(['I', have, X, cukes]) ->
+	X;

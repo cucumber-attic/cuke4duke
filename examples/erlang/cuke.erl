a87485d9456f21c015761f90fad8bba494ca3dd0
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-05-01 14:57:37 -0500

    Dennis hacking on a bench

--- examples/erlang/cuke.erl
+++ examples/erlang/cuke.erl
@@ -4,13 +4,18 @@
 
 % Gherkin: Given I have 7 cukes
 % given(['I', have, 7, cukes])
-
 % [7]
+
+% Gherkin: Given I have 3 wives in Sweden
+
 given(['I', have, X, cukes]) ->
 	X;
 
 given(['I', have, X, wives, in, Y]) ->
 	X.
 		
-then(['I', have, X, cukes]) ->
+given(['I', have, X, cukes]) when X <= 6 ->
 	X;
+	
+function_name(X) when X > 0 -> 
+   ;
